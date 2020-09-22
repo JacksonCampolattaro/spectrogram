502a1530f38d1d2e5dd7ada8e5c64a30d5a1b88b
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-22 17:27:01 -0400

    Add Audio::System class

---
+++ src/Audio/System.h
@@ -0,0 +1,17 @@
+#ifndef SPECTROGRAM_SYSTEM_H
+#define SPECTROGRAM_SYSTEM_H
+
+#include <rtaudio/RtAudio.h>
+
+namespace Audio {
+
+    class System {
+
+        RtAudio _rtAudio;
+
+    };
+
+}
+
+
+#endif //SPECTROGRAM_SYSTEM_H
