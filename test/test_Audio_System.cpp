beaf6b45f9bf65f3aaea46d19e50e1a0ad37fb18
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-23 00:04:19 -0400

    Add test which uses PortAudio

--- test/test_Audio_System.cpp
+++ test/test_Audio_System.cpp
@@ -3,16 +3,35 @@
 
 #include <Audio/System.h>
 
+#include <portaudio.h>
+#include <soundio/soundio.h>
+
 TEST_CASE("Connecting to system audio", "[Audio::System]") {
-
-    Audio::System system;
-
-    std::cout << system._rtAudio.getDeviceCount() << " Devices" << std::endl;
+//
+//    Audio::System system;
+//
+//    std::cout << system._rtAudio.getDeviceCount() << " Devices" << std::endl;
 }
 
 TEST_CASE("Printing out all devices", "[Audio::System]") {
+//
+//    Audio::System system;
+//
+//    system.devices();
+}
 
-    Audio::System system;
+TEST_CASE("Printing out all devices with portaudio", "[Audio::System]") {
+//
+//    Pa_Initialize();
+//    std::cout << Pa_GetDeviceCount() << std::endl;
+//    for (int i = 0; i < Pa_GetDeviceCount(); ++i) {
+//        std::cout << Pa_GetDeviceInfo(i)->name << std::endl;
+//    }
+//
+}
 
-    system.devices();
+TEST_CASE("Printing out all devices with libsoundio", "[Audio::System]") {
+
+    auto soundio = soundio_create();
+    std::cout << soundio_output_device_count(soundio) << std::endl;
 }
