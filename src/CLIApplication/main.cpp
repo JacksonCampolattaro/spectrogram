24389849d78dd7ef33e966368d584b2a5292267a
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-29 17:58:22 -0400

    Use a shared_ptr to enable polymorphism

--- src/CLIApplication/main.cpp
+++ src/CLIApplication/main.cpp
@@ -2,15 +2,17 @@
 #include <Spectrogram/Audio/System/LibSoundio.h>
 
 #include <iostream>
+#include <memory>
 
 using namespace Spectrogram::Audio;
 
 int main() {
 
-    System::LibSoundio system{};
+    // Initialize the system
+    std::shared_ptr<System::System> system = std::make_shared<System::LibSoundio>();
 
     std::cout << "Devices:" << std::endl;
-    for (auto device : system.getDevices())
+    for (auto device : system->getDevices())
         std::cout << device << std::endl;
 
     for (;;) {
