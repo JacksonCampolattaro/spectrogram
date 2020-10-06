0eb02e7a9675eda61900dadfba1784e2e3bde0da
Jackson Campolattaro <jackcamp@vt.edu>
2020-10-06 16:49:37 -0400

    Outline the "backend" class

--- src/Spectrogram/Audio/Backend/Backend.h
+++ src/Spectrogram/Audio/Backend/Backend.h
@@ -2,13 +2,14 @@
 #define SPECTROGRAM_BACKEND_H
 
 #include <Spectrogram/Audio/Buffer.h>
-#include <Spectrogram/Audio/Device.h>
+#include <Spectrogram/Audio/DeviceList.h>
 
 #include <functional>
 
 namespace Spectrogram::Audio::Backend {
 
-    class Backend {
+    template <class Interface>
+    class Backend : public Interface {
     public:
 
         typedef Sample Sample;
@@ -17,13 +18,11 @@ namespace Spectrogram::Audio::Backend {
         typedef std::function<void(const std::vector<Device> &)> DevicesChangedCallback;
         typedef std::function<void(Buffer)> NewBufferCallback;
 
-        virtual std::vector<Device> &devices() = 0;
-        virtual void start(Device device, NewBufferCallback callback) = 0;
+        virtual DeviceList &devices() = 0;
+        virtual void start(const Device &device, NewBufferCallback callback) = 0;
         virtual void stop() = 0;
 
     private:
-
-        DevicesChangedCallback _devicesChanged;
 
     };
 }
