09acb0388486aed73323a578cbc0ceb9ab71e237
Therese Hempenius <thereseh@vt.edu>
2020-10-09 13:32:19 -0400

    Updated namespace and changed to typedefs Sample and Channel. Added a prefix to FindLibsoundio.cmake

--- test/test_Fourier_Process.cpp
+++ test/test_Fourier_Process.cpp
@@ -4,30 +4,32 @@
 #include <iostream>
 #include <vector>
 
-TEST_CASE("Test constructors", "[Fourier::Processor]") {
+using namespace Spectrogram::Fourier;
 
-    Fourier::Processor a(5); 
+TEST_CASE("Test constructors", "[Processor]") {
+
+    Processor a(5); 
     REQUIRE(a.getSampleSize() == 5);
     REQUIRE(a.getOutputSize() == 3);
     
 
-    Fourier::Processor b;
+    Processor b;
     a = b;
     REQUIRE(b.getSampleSize() == 2048);
     REQUIRE(b.getOutputSize() == 1025);
     REQUIRE(a.getSampleSize() == 2048);
     REQUIRE(a.getOutputSize() == 1025);
 
-    Fourier::Processor c(a); 
+    Processor c(a); 
     REQUIRE(c.getSampleSize() == 2048);
     REQUIRE(c.getOutputSize() == 1025);
 
-    REQUIRE_THROWS_AS(Fourier::Processor(0), std::invalid_argument);
+    REQUIRE_THROWS_AS(Processor(0), std::invalid_argument);
 
 }
 
-TEST_CASE("Test setters", "[Fourier::Processor]") {
-    Fourier::Processor a(1024);
+TEST_CASE("Test setters", "[Processor]") {
+    Processor a(1024);
     REQUIRE(a.getSampleSize() == 1024);
     REQUIRE(a.getOutputSize() == 513);
 
@@ -37,7 +39,7 @@ TEST_CASE("Test setters", "[Fourier::Processor]") {
 
 }
 
-TEST_CASE("Test math", "[Fourier::Processor]") {
+TEST_CASE("Test math", "[Processor]") {
     
     int aFreq = 3; // Hz
     int aSampleRate = 100; 
@@ -48,7 +50,7 @@ TEST_CASE("Test math", "[Fourier::Processor]") {
         aSamples[t] =  sin(2*M_PI*aFreq*((double)t/aSampleRate));
     }
 
-    Fourier::Processor a(aSampleRate); 
+    Processor a(aSampleRate); 
     std::vector<float> aProcessedWindow = a.compute(aSamples);
     std::cout << "freq bin 3 should be 1 since the freq = 3:\n";
     std::cout << "output:\n";
@@ -68,7 +70,7 @@ TEST_CASE("Test math", "[Fourier::Processor]") {
         bSamples[t] =  sin(2*M_PI*bFreq*((double)t/bSampleRate));
     }
 
-    Fourier::Processor b(bSampleRate); 
+    Processor b(bSampleRate); 
     std::vector<float> bProcessedWindow = b.compute(bSamples);
     REQUIRE(bProcessedWindow[bFreq] == 1);
 
@@ -82,7 +84,7 @@ TEST_CASE("Test math", "[Fourier::Processor]") {
         cSamples[t] =  sin(2*M_PI*cFreq*((double)t/bSampleRate));
     }
 
-    Fourier::Processor c(bSampleRate); 
+    Processor c(bSampleRate); 
     std::vector<float> cProcessedWindow = c.compute(cSamples);
     REQUIRE(cProcessedWindow[cFreq] == 1);
 
@@ -96,7 +98,7 @@ TEST_CASE("Test math", "[Fourier::Processor]") {
         dSamples[t] = bSamples[t] + cSamples[t] + sin(2*M_PI*dFreq*((double)t/bSampleRate));
     }
 
-    Fourier::Processor d(bSampleRate); 
+    Processor d(bSampleRate); 
     std::vector<float> dProcessedWindow = d.compute(dSamples);
     REQUIRE(dProcessedWindow[bFreq] == 1);
     REQUIRE(dProcessedWindow[cFreq] == 1);
