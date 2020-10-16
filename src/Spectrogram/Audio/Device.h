038d26c52a49943f243de0ba1f3cbf3a8d15a4ba
Jackson Campolattaro <jackcamp@vt.edu>
2020-10-16 15:50:33 -0400

    Document device ostream operator

--- src/Spectrogram/Audio/Device.h
+++ src/Spectrogram/Audio/Device.h
@@ -20,6 +20,12 @@ namespace Spectrogram::Audio {
         bool isDefault;
         size_t channelCount;
 
+        /**
+         * @brief This allows directly writing the device's info to an output stream, useful for debugging
+         * @param os The output stream to write to
+         * @param device The device to write
+         * @return the output stream (for chained invocations)
+         */
         friend std::ostream &operator<<(std::ostream &os, const Device &device) {
 
             os << device.id << ": ";
