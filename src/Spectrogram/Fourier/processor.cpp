4bdd30f81e82187af9d8e34a091613edba02d5a9
Jackson Campolattaro <jackcamp@vt.edu>
2020-10-16 23:21:39 -0400

    Apply linter to Processor class

--- src/Spectrogram/Fourier/processor.cpp
+++ src/Spectrogram/Fourier/processor.cpp
@@ -13,168 +13,142 @@
 
 namespace Spectrogram::Fourier {
 
-Processor::Processor(int wSize) : windowSize(wSize)
-{
-    validateWindowSize(wSize);
-    setMembers(wSize);
-}
-
-Processor::Processor(const Processor &rhs)
-{
-    copyProcessor(rhs);
-}
-
-void Processor::validateWindowSize(const int& wSize) const
-{
-    if(wSize < 1)
-    {
-        throw std::invalid_argument("ERROR: windowSize = " + std::to_string(wSize) + ", must be larger than 1.");
+    Processor::Processor(int wSize) : windowSize(wSize) {
+        validateWindowSize(wSize);
+        setMembers(wSize);
     }
-}
 
-Processor::~Processor()
-{
-    cleanUp();
-}
-
-void Processor::cleanUp()
-{
-    fftw_destroy_plan(plan);
-    fftw_free(in);
-    fftw_free(out);
-}
-
-Processor& Processor::operator=(const Processor &rhs)
-{
-    if(this != &rhs)
-    {
-        cleanUp();
+    Processor::Processor(const Processor &rhs) {
         copyProcessor(rhs);
     }
-    return *this;
-}
 
-int Processor::calcComplexSize() const
-{
-    return  windowSize/2 + 1;
-}
+    void Processor::validateWindowSize(const int &wSize) const {
+        if (wSize < 1) {
+            throw std::invalid_argument("ERROR: windowSize = " + std::to_string(wSize) + ", must be larger than 1.");
+        }
+    }
 
-void Processor::copyProcessor(const Processor &rhs)
-{
-    setMembers(rhs.windowSize);
-}
-
-void Processor::setMembers(int newWindowSize)
-{
-    windowSize = newWindowSize;
-    complexSize = calcComplexSize();
-    in = fftw_alloc_real(windowSize);
-    out = fftw_alloc_complex(complexSize);
-    plan = fftw_plan_dft_r2c_1d(windowSize, in, out, FFTW_ESTIMATE);
-}
-
-void Processor::setSampleSize(int newSampleSize)
-{
-    validateWindowSize(newSampleSize);
-    if (newSampleSize != windowSize)
-    {
+    Processor::~Processor() {
         cleanUp();
-        setMembers(newSampleSize);
     }
-}
 
-int Processor::getSampleSize() const
-{
-    return windowSize;
-}
-
-int Processor::getOutputSize() const
-{
-    return complexSize;
-}
-
-Audio::Channel Processor::compute(const Audio::Channel& samples)
-{
-    in = applyHanningWindow(samples);
-
-    out = executeFFT();
-
-    Audio::Channel processedOutput = calcMagnitudeInDB();
-    processedOutput = normalize(processedOutput);
-
-    return processedOutput;
-}
-
-fftw_complex* Processor::executeFFT()
-{
-    fftw_execute(plan);
-
-    return out;
-}
-
-Audio::Sample Processor::hann(const int &i, const int &n)
-{
-    return ( 1.0 - cos(2.0 * M_PI * i/ (n-1)) ) * 0.5;
-}
-
-double* Processor::applyHanningWindow(const Audio::Channel& samples)
-{
-    // The following for loop is a modification of the two for loops provided in URL
-    //https://stackoverflow.com/questions/25061441/correct-way-to-implement-windowing/25061729#25061729?newreg=2a07fefa30ba41e1a286e025123513f8
-    for (int i = 0; i < windowSize; i++)
-    {
-        in[i] = samples[i] * hann(i,windowSize);
+    void Processor::cleanUp() {
+        fftw_destroy_plan(plan);
+        fftw_free(in);
+        fftw_free(out);
     }
-    return in;
-}
+
+    Processor &Processor::operator=(const Processor &rhs) {
+        if (this != &rhs) {
+            cleanUp();
+            copyProcessor(rhs);
+        }
+        return *this;
+    }
+
+    int Processor::calcComplexSize() const {
+        return windowSize / 2 + 1;
+    }
+
+    void Processor::copyProcessor(const Processor &rhs) {
+        setMembers(rhs.windowSize);
+    }
+
+    void Processor::setMembers(int newWindowSize) {
+        windowSize = newWindowSize;
+        complexSize = calcComplexSize();
+        in = fftw_alloc_real(windowSize);
+        out = fftw_alloc_complex(complexSize);
+        plan = fftw_plan_dft_r2c_1d(windowSize, in, out, FFTW_ESTIMATE);
+    }
+
+    void Processor::setSampleSize(int newSampleSize) {
+        validateWindowSize(newSampleSize);
+        if (newSampleSize != windowSize) {
+            cleanUp();
+            setMembers(newSampleSize);
+        }
+    }
+
+    int Processor::getSampleSize() const {
+        return windowSize;
+    }
+
+    int Processor::getOutputSize() const {
+        return complexSize;
+    }
+
+    Audio::Channel Processor::compute(const Audio::Channel &samples) {
+        in = applyHanningWindow(samples);
+
+        out = executeFFT();
+
+        Audio::Channel processedOutput = calcMagnitudeInDB();
+        processedOutput = normalize(processedOutput);
+
+        return processedOutput;
+    }
+
+    fftw_complex *Processor::executeFFT() {
+        fftw_execute(plan);
+
+        return out;
+    }
+
+    Audio::Sample Processor::hann(const int &i, const int &n) {
+        return (1.0 - cos(2.0 * M_PI * i / (n - 1))) * 0.5;
+    }
+
+    double *Processor::applyHanningWindow(const Audio::Channel &samples) {
+        // The following for loop is a modification of the two for loops provided in URL
+        //https://stackoverflow.com/questions/25061441/correct-way-to-implement-windowing/25061729#25061729?newreg=2a07fefa30ba41e1a286e025123513f8
+        for (int i = 0; i < windowSize; i++) {
+            in[i] = samples[i] * hann(i, windowSize);
+        }
+        return in;
+    }
 
 // Magnitude equation adapted from stackoverflow user Hartmut Pfitzinger
 // https://stackoverflow.com/questions/21283144/generating-correct-spectrogram-using-fftw-and-window-function
-Audio::Channel Processor::calcMagnitudeInDB()
-{
-    Audio::Channel magnitudeDB(complexSize, 0);
-    // Since only have half the complex values multiply by 2, (do not have conjugates)
-    // And divide by sample size to get ratio
-    float scale = 2.0 / windowSize;
-    for (int i = 0; i < complexSize; i++)
-    {
-        // Power spectral density is |real|^2 + |imag|^2
-        magnitudeDB[i] = pow(out[i][REAL] * scale, 2) + pow(out[i][IMAG] * scale, 2);
+    Audio::Channel Processor::calcMagnitudeInDB() {
+        Audio::Channel magnitudeDB(complexSize, 0);
+        // Since only have half the complex values multiply by 2, (do not have conjugates)
+        // And divide by sample size to get ratio
+        float scale = 2.0 / windowSize;
+        for (int i = 0; i < complexSize; i++) {
+            // Power spectral density is |real|^2 + |imag|^2
+            magnitudeDB[i] = pow(out[i][REAL] * scale, 2) + pow(out[i][IMAG] * scale, 2);
 
-        // Convert from power to decibel for color intensity display
-        magnitudeDB[i] = convertToDb(magnitudeDB[i]);
+            // Convert from power to decibel for color intensity display
+            magnitudeDB[i] = convertToDb(magnitudeDB[i]);
+        }
+
+        return magnitudeDB;
     }
 
-    return magnitudeDB;
-}
+    Audio::Sample Processor::convertToDb(const Audio::Sample &val) {
+        // Epsilon is to make sure we don't do log(0)
+        Audio::Sample dBVal = POWER_TO_DB_MULT * std::log10(val + EPSILON);
 
-Audio::Sample Processor::convertToDb(const Audio::Sample &val)
-{
-    // Epsilon is to make sure we don't do log(0)
-    Audio::Sample dBVal = POWER_TO_DB_MULT * std::log10(val + EPSILON);
-
-    return dBVal;
-}
+        return dBVal;
+    }
 
 // TODO: if this is slowing down the processing too much, there is
 // a rougher way to normalize without having to find the min and max
-Audio::Channel& Processor::normalize(Audio::Channel& v)
-{
-    Audio::Sample max = *std::max_element(v.begin(), v.end());
-    Audio::Sample min = *std::min_element(v.begin(), v.end());
+    Audio::Channel &Processor::normalize(Audio::Channel &v) {
+        Audio::Sample max = *std::max_element(v.begin(), v.end());
+        Audio::Sample min = *std::min_element(v.begin(), v.end());
 
-    if (max == min)
-    {
-        throw std::domain_error("ERROR: " + std::to_string(max) + ", all the values are the same in this Channel.");
-    }
+        if (max == min) {
+            throw std::domain_error("ERROR: " + std::to_string(max) + ", all the values are the same in this Channel.");
+        }
 
-    // Normalization adapted from user Alex I from stackoverflow
-    // https://stackoverflow.com/questions/21283144/generating-correct-spectrogram-using-fftw-and-window-function
-    for(auto it = v.begin(); it != v.end(); ++it)
-    {
-        *it = (*it - min) / (max - min);
+        // Normalization adapted from user Alex I from stackoverflow
+        // https://stackoverflow.com/questions/21283144/generating-correct-spectrogram-using-fftw-and-window-function
+        for (auto it = v.begin(); it != v.end(); ++it) {
+            *it = (*it - min) / (max - min);
+        }
+        return v;
     }
-    return v;
-}
 
 }
--- src/Spectrogram/Fourier/processor.h
+++ src/Spectrogram/Fourier/processor.h
@@ -7,60 +7,76 @@
 
 namespace Spectrogram::Fourier {
 
-class Processor {
+    class Processor {
     public:
         // Parameterized
-        Processor(int wSize = 2048);
+        explicit Processor(int wSize = 2048);
+
         // Copy Constructor
         Processor(const Processor &rhs);
+
         // Destructor
         ~Processor();
+
         // Assignment operator
-        Processor& operator=(const Processor &rhs);
+        Processor &operator=(const Processor &rhs);
+
         // Compute the spectrogram output vector from input data
         // Input: Vector of floats from audio stream, size of windowSize
         // Output: Vector of floats half the size of the input in decibels that 
         //         have been normalized to be between 0 and 1
-        Audio::Channel compute(const Audio::Channel& samples);
+        Audio::Channel compute(const Audio::Channel &samples);
+
         // Returns the windowSize, which is the input array length
         int getSampleSize() const;
+
         // Returns the output array size that is produced after processing the input
         int getOutputSize() const;
+
         // Sets the windowSize to something else
         void setSampleSize(int newSampleSize);
 
     private:
         // Returns the output array size
         int calcComplexSize() const;
+
         // Executes the FFT using FFTW3 library
-        fftw_complex* executeFFT();
+        fftw_complex *executeFFT();
+
         // Applies hanning window to input data and copies the windowed data to 'in' array
-        double* applyHanningWindow(const Audio::Channel& samples);
+        double *applyHanningWindow(const Audio::Channel &samples);
+
         // Gets the magnitude of the complex values and converts to decibels
         Audio::Channel calcMagnitudeInDB();
+
         // Converts magnitude to decibels
         Audio::Sample convertToDb(const Audio::Sample &val);
+
         // Normalizes the data to be between 0 and 1
-        Audio::Channel& normalize(Audio::Channel& v);
+        Audio::Channel &normalize(Audio::Channel &v);
 
         // Calculate Hann
-        float hann(const int&, const int&);
+        float hann(const int &, const int &);
+
         // Copy another processor's members to this object
         void copyProcessor(const Processor &rhs);
+
         // returns true if window size is greater then 1
         // throws a runtime error if window size is less then 1
-        void validateWindowSize(const int& wSize) const;
+        void validateWindowSize(const int &wSize) const;
+
         // Sets this objects members
         void setMembers(int newWindowSize);
+
         // Destroy the plan and free the arrays
         void cleanUp();
 
         int complexSize; // Output array size
         int windowSize; // Provided by audio stream, the amount of floats inside the sample
         double *in; // size is dependent on the window size
-        fftw_complex* out; // complex output array
+        fftw_complex *out; // complex output array
         fftw_plan plan; // Plan to perform the FFT on the real data to complex
-};
+    };
 
 }
 
