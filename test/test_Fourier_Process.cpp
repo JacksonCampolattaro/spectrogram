ef5e411c302f9c9fd90e89e0a56887268f136b4b
Therese Hempenius <thereseh@vt.edu>
2020-10-24 12:56:54 -0400

    Removed normalization from processor class

--- test/test_Fourier_Process.cpp
+++ test/test_Fourier_Process.cpp
@@ -6,6 +6,22 @@
 
 using namespace Spectrogram::Fourier;
 
+Spectrogram::Audio::Channel& normalize(Spectrogram::Audio::Channel &v) {
+    Spectrogram::Audio::Sample max = *std::max_element(v.begin(), v.end());
+    Spectrogram::Audio::Sample min = *std::min_element(v.begin(), v.end());
+
+    if (max == min) {
+        throw std::domain_error("ERROR: " + std::to_string(max) + ", all the values are the same in this Channel.");
+    }
+
+    // Normalization adapted from user Alex I from stackoverflow
+    // https://stackoverflow.com/questions/21283144/generating-correct-spectrogram-using-fftw-and-window-function
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        *it = (*it - min) / (max - min);
+    }
+    return v;
+}
+
 TEST_CASE("Test constructors", "[Processor]") {
 
     Processor a(5); 
@@ -40,7 +56,7 @@ TEST_CASE("Test setters", "[Processor]") {
 }
 
 TEST_CASE("Test math", "[Processor]") {
-    
+
     int aFreq = 3; // Hz
     int aSampleRate = 100; 
     std::vector<float> aSamples(aSampleRate, 0);
@@ -52,6 +68,7 @@ TEST_CASE("Test math", "[Processor]") {
 
     Processor a(aSampleRate); 
     std::vector<float> aProcessedWindow = a.compute(aSamples);
+    aProcessedWindow = normalize(aProcessedWindow);
     std::cout << "freq bin 3 should be 1 since the freq = 3:\n";
     std::cout << "output:\n";
     for(float db : aProcessedWindow)
@@ -72,6 +89,7 @@ TEST_CASE("Test math", "[Processor]") {
 
     Processor b(bSampleRate); 
     std::vector<float> bProcessedWindow = b.compute(bSamples);
+    bProcessedWindow = normalize(bProcessedWindow);
     REQUIRE(bProcessedWindow[bFreq] == 1);
 
     /////////////////////////////////////////////////////////////////
@@ -86,6 +104,7 @@ TEST_CASE("Test math", "[Processor]") {
 
     Processor c(bSampleRate); 
     std::vector<float> cProcessedWindow = c.compute(cSamples);
+    cProcessedWindow = normalize(cProcessedWindow);
     REQUIRE(cProcessedWindow[cFreq] == 1);
 
     //////////////////////////////////////////////////////////////////
@@ -100,6 +119,7 @@ TEST_CASE("Test math", "[Processor]") {
 
     Processor d(bSampleRate); 
     std::vector<float> dProcessedWindow = d.compute(dSamples);
+    dProcessedWindow = normalize(dProcessedWindow);
     REQUIRE(dProcessedWindow[bFreq] == 1);
     REQUIRE(dProcessedWindow[cFreq] == 1);
     REQUIRE(dProcessedWindow[dFreq] == 1);
