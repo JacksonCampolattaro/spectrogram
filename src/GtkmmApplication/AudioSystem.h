f91c10b1759e7337be83c6991918c895a953f11a
Jackson Campolattaro <jackcamp@vt.edu>
2020-10-30 20:20:13 -0400

    Add some comments to the AudioSystem class

--- src/GtkmmApplication/AudioSystem.h
+++ src/GtkmmApplication/AudioSystem.h
@@ -16,9 +16,14 @@ public:
     explicit AudioSystem(std::unique_ptr<Backend::Backend> backend) :
             Event(std::move(backend)) {
 
+        // Gtkmm's Dispatcher lets us send signals between threads
+        // this means checkForNewData will be run in the main event loop
         _dispatcher.connect(sigc::mem_fun(*this, &AudioSystem::checkForNewData));
 
+        // The start slot will simply invoke the super class's start method
         start = sigc::mem_fun<const Device &, std::chrono::milliseconds, size_t>(*this, &Event::start);
+
+        // The stop slot will also invoke the super's stop method
         stop = sigc::mem_fun(*this, &Event::stop);
 
     }
@@ -33,12 +38,15 @@ public:
 private:
 
     void notifyNewData() override {
+
+        // This tells the main thread to check if there's a buffer's worth of new data available.
+        // If there is, processNewData will be called!
         _dispatcher.emit();
     }
 
     void processNewData(const Buffer &buffer) override {
 
-        // Allow the buffer to be processed
+        // The newBuffer signal tells whatever visualizer you're using to update
         newBuffer.emit(buffer);
     }
 
