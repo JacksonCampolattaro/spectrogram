ba68c46aebf44521aacfa01d971ad33b98b98c20
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-01 15:33:33 -0500

    Use buffer constructor in test

--- test/test_Audio_System.cpp
+++ test/test_Audio_System.cpp
@@ -14,13 +14,11 @@ using namespace Spectrogram::Audio;
 TEST_CASE("Dummy devices", "[Dummy]") {
 
     auto system = System::Blocking(std::make_unique<Backend::Dummy>(440));
+    auto device = system.devices()[0];
 
-    system.start(system.devices()[0], std::chrono::seconds(2));
+    system.start(device, std::chrono::seconds(2));
 
-    Buffer buffer;
-    buffer.channels().resize(system.devices()[0].channelCount);
-    for (auto &channel : buffer.channels())
-        channel.resize(200);
+    Buffer buffer(device, 200);
 
     system.fillBuffer(buffer);
     for (auto channel : buffer.channels()) {
