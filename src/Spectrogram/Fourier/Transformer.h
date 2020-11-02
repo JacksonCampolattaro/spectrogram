ac6915f11d7fb64f5d03a372c7a4012b90d24427
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-01 21:51:04 -0500

    Add a preliminary implementation of the transform method

--- src/Spectrogram/Fourier/Transformer.h
+++ src/Spectrogram/Fourier/Transformer.h
@@ -3,12 +3,26 @@
 
 #include <Spectrogram/Audio/Buffer.h>
 #include <Spectrogram/Fourier/FrequencyDomainBuffer.h>
+#include <Spectrogram/Fourier/processor.h>
 
 namespace Spectrogram::Fourier {
 
     FrequencyDomainBuffer transform(const Audio::Buffer &buffer) {
+        static Processor processor;
+
+        std::vector<std::vector<Intensity>> intensities;
+        for (const auto &channel : buffer.channels())
+            intensities.push_back(processor.compute(channel));
 
         FrequencyDomainBuffer frequencyDomainBuffer;
+        for (size_t index = 0; index < intensities.size(); ++index) {
+
+            // FIXME: This is temporary
+            Frequency frequency = index;
+
+            frequencyDomainBuffer.emplace(std::make_pair(frequency, intensities[index]));
+
+        }
 
         return frequencyDomainBuffer;
     }
