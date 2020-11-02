24e5948202897f000e7710423248a3cb9931fca1
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-01 20:49:28 -0500

    Declare a helper function "transform", eventually to be replaced with a class

---
+++ src/Spectrogram/Fourier/Transformer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jackcamp on 11/1/20.
+//
+
+#include "Transformer.h"
---
+++ src/Spectrogram/Fourier/Transformer.h
@@ -0,0 +1,18 @@
+#ifndef SPECTROGRAM_TRANSFORMER_H
+#define SPECTROGRAM_TRANSFORMER_H
+
+#include <Spectrogram/Audio/Buffer.h>
+#include <Spectrogram/Fourier/FrequencyDomainBuffer.h>
+
+namespace Spectrogram::Fourier {
+
+    FrequencyDomainBuffer transform(const Audio::Buffer &buffer) {
+
+        FrequencyDomainBuffer frequencyDomainBuffer;
+
+        return frequencyDomainBuffer;
+    }
+
+}
+
+#endif //SPECTROGRAM_TRANSFORMER_H
