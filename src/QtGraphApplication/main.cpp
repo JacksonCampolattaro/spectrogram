88061731a7f4ee3b51031f3d7ceeb1e12a2f56f4
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-07 14:26:42 -0500

    Outline structure of main

--- src/QtGraphApplication/main.cpp
+++ src/QtGraphApplication/main.cpp
@@ -1,11 +1,25 @@
 #include <QApplication>
 #include "GraphGui.h"
+#include "QtAudioSystem.h"
 
 
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
+
+	// Create the GUI
 	GraphGui gui;
 	gui.show();
+
+	// Create the audio system
+    QtAudioSystem audioSystem(std::make_unique<Backend::Soundio>());
+
+	// Link up the signals
+	// TODO
+
+	// The GUI doesn't have buttons yet, so we'll have to manually start the backend
+	// TODO
+
+	// Start the application
 	return app.exec();
 }
