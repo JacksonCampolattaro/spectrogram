4e9e80b9812833b188ccff292e66577f4453c022
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-06 21:13:19 -0500

    Begin creating Audio system object designed for Qt signals

---
+++ src/QtGraphApplication/QtAudioSystem.h
@@ -0,0 +1,11 @@
+#ifndef SPECTROGRAM_QTAUDIOSYSTEM_H
+#define SPECTROGRAM_QTAUDIOSYSTEM_H
+
+#include <Spectrogram/Audio/System/Event.h>
+
+class QtAudioSystem {
+
+};
+
+
+#endif //SPECTROGRAM_QTAUDIOSYSTEM_H
