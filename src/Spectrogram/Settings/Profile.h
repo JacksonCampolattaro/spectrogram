647937159d35a7c4c8d2bc5a8c5bf1be5212c4a7
JacksonCampolattaro <jackcamp@vt.edu>
2020-11-24 16:50:41 -0500

    Begin creating a Settings::Profile object

---
+++ src/Spectrogram/Settings/Profile.h
@@ -0,0 +1,18 @@
+
+#ifndef SPECTROGRAM_PROFILE_H
+#define SPECTROGRAM_PROFILE_H
+
+#include <Spectrogram/Settings/Settings.h>
+
+namespace Settings {
+
+    class Profile {
+    public:
+
+        int colorScheme;
+
+    };
+
+}
+
+#endif //SPECTROGRAM_PROFILE_H
