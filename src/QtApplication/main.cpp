e872779ca4733b26a087bf639508ae64dfb47688
Jackson Campolattaro <jackcamp@vt.edu>
2020-11-30 20:35:00 -0500

    Auto-create and load config file

--- src/QtApplication/main.cpp
+++ src/QtApplication/main.cpp
@@ -1,24 +1,47 @@
 #include <QApplication>
 #include <QtApplication/QtAudioSystem.h>
+
+#include <fstream>
+
 #include "QtApplication.h"
 
 
-int main(int argc, char* argv[])
-{
-	QApplication app(argc, argv);
+int main(int argc, char *argv[]) {
 
-	QtApplication gui;
-	gui.show();
+    auto configPath = "spectrogram.rc";
 
-	QtAudioSystem audioSystem(std::make_unique<Backend::Soundio>());
-	gui.updateSources(audioSystem.devices()); // Manually tell the gui what devices are available
-	QObject::connect(&gui, &QtApplication::startSignal,
+    // Create a default settings profile
+    Settings::Profile settings;
+
+    // Attempt to open the config file in the default location
+    std::ifstream configFile(configPath);
+    if (!configFile) {
+        configFile.close();
+
+        // Missing config file, create one
+        std::ofstream newConfig(configPath);
+        newConfig << settings;
+        newConfig.close();
+    } else {
+
+        // Load settings from the config file
+        configFile >> settings;
+    }
+
+    QApplication app(argc, argv);
+
+    QtApplication gui;
+    gui.show();
+
+    QtAudioSystem audioSystem(std::make_unique<Backend::Soundio>());
+    gui.updateSources(audioSystem.devices()); // Manually tell the gui what devices are available
+    QObject::connect(&gui, &QtApplication::startSignal,
                      &audioSystem, &QtAudioSystem::startSlot);
-	QObject::connect(&gui, &QtApplication::stopSignal,
+    QObject::connect(&gui, &QtApplication::stopSignal,
                      &audioSystem, &QtAudioSystem::stopSlot);
 
-	QObject::connect(&audioSystem, &QtAudioSystem::newBufferSignal,
+    QObject::connect(&audioSystem, &QtAudioSystem::newBufferSignal,
                      &gui, &QtApplication::updateSpectrogram);
 
-	return app.exec();
+    return app.exec();
 }
